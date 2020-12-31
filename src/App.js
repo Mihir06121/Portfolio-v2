0093e96cd85c22cf1a99c150ccaece17f8fb6c5d
Mihir06121 <mp06121@gmail.com>
2020-12-31 14:35:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import './App.css';
-import {Switch, Route} from 'react-router-dom'
+import { Route} from 'react-router-dom'
 import Home from '../src//components/pages/Home'
 import About from '../src//components/pages/About'
 import Projects from '../src//components/pages/Projects'
@@ -13,13 +13,11 @@ import Footer from '../src//components/Footer'
 const App = () => {
   return (
     <>
-      <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/about" component={About} />
         <Route exact path="/projects" component={Projects} />
         <Route exact path="/contact-me" component={Contact} />
-      </Switch>
-      <Footer/>
+        <Footer/>
     </>
   );
 }
