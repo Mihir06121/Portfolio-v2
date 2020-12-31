68a85a62e47ff76aba302f68975227584b2bb073
Mihir06121 <mp06121@gmail.com>
2020-12-31 13:21:47 +0530

    Updates

---
+++ precache-manifest.e096fddf4f10c218e1325899640edfc0.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd84247456533c042be7b9d0310e8ecc",
+    "url": "/portfolio-v2/index.html"
+  },
+  {
+    "revision": "41ada77039f4d78e4310",
+    "url": "/portfolio-v2/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "d5a8ab99eea4f2493bc7",
+    "url": "/portfolio-v2/static/css/main.37e415ae.chunk.css"
+  },
+  {
+    "revision": "41ada77039f4d78e4310",
+    "url": "/portfolio-v2/static/js/2.a24eede3.chunk.js"
+  },
+  {
+    "revision": "e60f6644a238030ec5fb7683004cdede",
+    "url": "/portfolio-v2/static/js/2.a24eede3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5a8ab99eea4f2493bc7",
+    "url": "/portfolio-v2/static/js/main.f3a9304a.chunk.js"
+  },
+  {
+    "revision": "3992b0d658be16fa687d",
+    "url": "/portfolio-v2/static/js/runtime-main.cdfd8695.js"
+  },
+  {
+    "revision": "cf5760a3a8c65a7ea54fecb51e19524d",
+    "url": "/portfolio-v2/static/media/blog_platform_01.cf5760a3.png"
+  },
+  {
+    "revision": "8108a60e065f32b498a57c326b2eaaa3",
+    "url": "/portfolio-v2/static/media/blog_platform_02_2.8108a60e.png"
+  },
+  {
+    "revision": "ad95d1cf81a02e900077bd1e5d3e07dc",
+    "url": "/portfolio-v2/static/media/blog_platform_03.ad95d1cf.png"
+  },
+  {
+    "revision": "40d31a89abda757be4aa33f96a84a942",
+    "url": "/portfolio-v2/static/media/blog_platform_04.40d31a89.png"
+  },
+  {
+    "revision": "30474b67f5c33d1c709fc46bd8c27c24",
+    "url": "/portfolio-v2/static/media/blog_platform_05.30474b67.png"
+  },
+  {
+    "revision": "05691499795e67a9bf56f8cdd7835e92",
+    "url": "/portfolio-v2/static/media/blog_platform_06.05691499.png"
+  },
+  {
+    "revision": "285fe1072f5ac67062b5ea2f5112b2e9",
+    "url": "/portfolio-v2/static/media/covid_tracker_01.285fe107.png"
+  },
+  {
+    "revision": "3fe6c935ca7fe5523486f753e7a8ca4e",
+    "url": "/portfolio-v2/static/media/drive_to_park.3fe6c935.png"
+  },
+  {
+    "revision": "3c7df0260d3a97a2c674b37eb4dcd47c",
+    "url": "/portfolio-v2/static/media/drive_to_park2.3c7df026.png"
+  },
+  {
+    "revision": "187bcef9bf70f29421936d9f4d576130",
+    "url": "/portfolio-v2/static/media/drive_to_park3.187bcef9.png"
+  },
+  {
+    "revision": "a91f94d280f5008ac78853280fd7eca0",
+    "url": "/portfolio-v2/static/media/drive_to_park4.a91f94d2.png"
+  },
+  {
+    "revision": "a12a0a5f584b1e9abad02d52918c51ce",
+    "url": "/portfolio-v2/static/media/drive_to_park5.a12a0a5f.png"
+  },
+  {
+    "revision": "c64c421369dec520d673f4aa136e8058",
+    "url": "/portfolio-v2/static/media/drms_01.c64c4213.png"
+  },
+  {
+    "revision": "5d9d516d4cfb8d18b3d9c8e32527379e",
+    "url": "/portfolio-v2/static/media/drms_02.5d9d516d.png"
+  },
+  {
+    "revision": "9e86a12589fe2459284375875f0ac5b6",
+    "url": "/portfolio-v2/static/media/drms_03.9e86a125.png"
+  },
+  {
+    "revision": "59334f9cf505b1de8bf093bb2e93b16b",
+    "url": "/portfolio-v2/static/media/drms_04.59334f9c.png"
+  },
+  {
+    "revision": "e106d28d1324ed30b2396146110aea63",
+    "url": "/portfolio-v2/static/media/image1.e106d28d.png"
+  },
+  {
+    "revision": "82ee73ef45c8d83561bb26c8c83a2c73",
+    "url": "/portfolio-v2/static/media/react_youtube.82ee73ef.png"
+  }
+]);
