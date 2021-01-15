35137cb7ddcaba9a84d0b71736167ad83b21b3c5
Mihir06121 <mp06121@gmail.com>
2021-01-15 14:58:08 +0530

    changes in Project section

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,40 +1,43 @@
 import React from 'react';
-import { Card, CardText, CardBody,
-    CardTitle, CardSubtitle} from 'reactstrap'
+import { Card, CardText, CardBody, CardTitle } from 'reactstrap'
 
 const Contact = () => {
     return (
         <section id="contact" className="pt-5">
             <div className="container">
                 <h1>Contact Me</h1>
-                <div className="row">
-                    <div className="col-sm">
+                <div className="row p-4">
+                    <div className="col-sm" data-aos="zoom-in" data-aos-delay="1000">
                         <Card>
                             <CardBody>
-                            <CardTitle tag="h5">Card title</CardTitle>
-                            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title 
-                                and make up the bulk of the card's content.</CardText>
+                            <CardTitle tag="h5">Phone</CardTitle>
+                            <i className="fa fa-phone fa-2x"></i>
+                            <CardText>7700983123</CardText>
                             </CardBody>
                         </Card>
                     </div>
-                    <div className="col-sm">
+                    <div className="col-sm" data-aos="zoom-in" data-aos-delay="1500">
                         <Card>
                             <CardBody>
-                            <CardTitle tag="h5">Card title</CardTitle>
-                            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title 
-                                and make up the bulk of the card's content.</CardText>
+                            <CardTitle tag="h5">Gmail</CardTitle>
+                            <i className="fab fa-google fa-2x"></i>
+                            <CardText>mp06121@gmail.com</CardText>
                             </CardBody>
                         </Card>
                     </div>
-                    <div className="col-sm">
+                    <div className="col-sm" data-aos="zoom-in" data-aos-delay="2000">
                         <Card>
                             <CardBody>
-                            <CardTitle tag="h5">Card title</CardTitle>
-                            <CardSubtitle tag="h6" className="mb-2 text-muted">Card subtitle</CardSubtitle>
-                            <CardText>Some quick example text to build on the card title 
-                                and make up the bulk of the card's content.</CardText>
+                            <CardTitle tag="h5">Instagam</CardTitle><i className="fab fa-instagram fa-2x"></i>
+                            <CardText><a className="mx-2 text-dark" href="https://www.instagram.com/_mihir.panchal_/" >_mihir.panchal_</a></CardText>
+                            </CardBody>
+                        </Card>
+                    </div>
+                    <div className="col-sm" data-aos="zoom-in" data-aos-delay="2500">
+                        <Card>
+                            <CardBody>
+                            <CardTitle tag="h5">Github</CardTitle><i className="fab fa-github fa-2x"></i>
+                            <CardText><a className="mx-2 text-dark" href="https://github.com/Mihir06121">Mihir06121</a></CardText>
                             </CardBody>
                         </Card>
                     </div>
