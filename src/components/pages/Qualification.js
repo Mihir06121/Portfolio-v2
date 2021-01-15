6ecc884e69f8a9123048db641dacb6f6aa221eb1
Mihir06121 <mp06121@gmail.com>
2021-01-16 01:40:52 +0530

    Qualifications Added

--- src/components/pages/Qualification.js
+++ src/components/pages/Qualification.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Qualification = () => {
     return (
-        <div id="qualification" className="p-5">
+        <div id="qualification" className="pt-5">
             <h1 align="center" >My Qualifications</h1>
-            <section className="p-5">
+            <section className="pt-5">
                 <div className="left-border m-3 pt-5">
                 <h1 className="pl-2">BSc.IT</h1>
                 <h4 className="pt-3 pl-3">Pursunig my BSc.IT degree form Shree L. R. Tiwari 
